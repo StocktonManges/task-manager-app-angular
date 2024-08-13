1d1744a5746d0fc1c1b6283ca98758d03262e7bf
StocktonManges <stocktonmanges@gmail.com>
2024-08-13 09:38:53 -0600

    Added dynamic rendering to user component and moved user images to a users directory in assets.

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
+  // Set an initial value for the signal function.
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  // Computed values when using signals.
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
 
+  // get imagePath() {
+  //   return 'assets/users/' + this.selectedUser.avatar;
+  // }
+
+  onSelectUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser.set(DUMMY_USERS[randomIndex]); // Setting a new value using signal.
+    // this.selectedUser = DUMMY_USERS[randomIndex];
+  }
 }
