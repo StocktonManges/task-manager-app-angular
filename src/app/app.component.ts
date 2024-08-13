05054544ecfc30c60824a0b06bbc199f21e50ec6
StocktonManges <stocktonmanges@gmail.com>
2024-08-13 08:31:32 -0600

    Created user and header components and added pictures to the assets folder.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
 
 @Component({
   // When compiled, the 'app-root' tag is found in index.html and
   // replaced with the app.component.html file.
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
